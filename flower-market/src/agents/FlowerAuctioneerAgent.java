aa0d181d61e3bd500e7245b33bfa1106a67d34d9
Jefersonalves <jefersonalvesf2009@gmail.com>
2017-05-24 20:42:09 -0300

    Create flower auctioneer agent

---
+++ flower-market/src/agents/FlowerAuctioneerAgent.java
@@ -0,0 +1,13 @@
+package agents;
+
+import jade.core.Agent;
+
+/**
+ * Agent responsible for initiating and conducting the auction
+ */
+public class FlowerAuctioneerAgent extends Agent{
+	protected void setup(){
+
+	}
+	
+}
