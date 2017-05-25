f2816a5b8ffb8c9cbd86a206d4244c95deadd735
Jefersonalves <jefersonalvesf2009@gmail.com>
2017-05-25 12:53:34 -0300

    Register flower buyer agent on yellow page

--- flower-market/src/agents/FlowerBuyerAgent.java
+++ flower-market/src/agents/FlowerBuyerAgent.java
@@ -1,12 +1,34 @@
 package agents;
 
 import jade.core.Agent;
+import jade.core.AID;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.*;
 
 /**
  * Flower buyer agent at auction
  */
 public class FlowerBuyerAgent extends Agent{
+
 	protected void setup(){
+		//Creating an agent entry on yellow page
+	  	DFAgentDescription description = new DFAgentDescription();
+	  	description.setName(getAID());
+	  		
+	  	//Adding a service
+	  	ServiceDescription service = new ServiceDescription();
+	  	service.setName("buyer");
+	  	service.setType("flower-buyer");
+	  	description.addServices(service);
+	  	
+	  	//agent trying to register on yellow page
+	  	try {
+	  		DFService.register(this, description);
+	  	}
+	  	catch (FIPAException fe) {
+	  		fe.printStackTrace();
+	  	}
 
 	}
 	
